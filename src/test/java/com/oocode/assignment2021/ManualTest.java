1c07031b1b0bdf948c5da5c0f339c5cf444ca4b6
Ankur Sand <sandankur@gmail.com>
2021-04-21 10:44:08 +0530

    Added a jUnit test case to test location as Mars . to ensure it does return an empty String . asserting the smae

--- src/test/java/com/oocode/assignment2021/ManualTest.java
+++ src/test/java/com/oocode/assignment2021/ManualTest.java
@@ -2,16 +2,25 @@ package com.oocode.assignment2021;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
 public class ManualTest {
-    @Test
+    //@Test
     public void test() throws IOException {
-        System.out.println(ChargerFinder.findBestCharger(
-                "https://tranquil-headland-85251.herokuapp.com",
-                64,
-                "CCS",
-                "Durham",
-                23));
+    	System.out.println(ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CCS", "Durham", 23));
+    }
+    
+    
+    @Test
+    public void testMarsLocation() throws IOException {
+    	String mars = ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CCS", "Mars", 23);
+    	
+    	// Asserting for an empty String when the location is Mars
+    	assertTrue(mars.isEmpty());
+    	
     }
 }

7824803ba0b47fd6f9b576104bff371d865911ea
Ankur Sand <sandankur@gmail.com>
2021-04-21 11:21:07 +0530

    Finished Question 1 to handle NoSuchElement Exception and return Empty String

--- src/test/java/com/oocode/assignment2021/ManualTest.java
+++ src/test/java/com/oocode/assignment2021/ManualTest.java
@@ -2,14 +2,12 @@ package com.oocode.assignment2021;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
 public class ManualTest {
-    //@Test
+    @Test
     public void test() throws IOException {
     	System.out.println(ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CCS", "Durham", 23));
     }

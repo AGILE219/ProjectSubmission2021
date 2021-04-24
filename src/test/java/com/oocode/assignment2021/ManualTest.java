841e773645769e3efa63ae2a5aa6609d0cc98f2f
Ankur Sand <sandankur@gmail.com>
2021-04-24 19:28:54 +0530

    Added a test case to check for the result which is based on the type of the charager

--- src/test/java/com/oocode/assignment2021/ManualTest.java
+++ src/test/java/com/oocode/assignment2021/ManualTest.java
@@ -7,19 +7,36 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 
 public class ManualTest {
-    @Test
+   
+	// Default test case to check the system is working as expected and showing the expected result
+	// @Test
     public void test() throws IOException {
     	System.out.println(ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CCS", "Durham", 23));
     }
+   
     
-    
-    @Test
+	//Question -1 , This test case is to check for the invalid location, which is not supported by the application. 
+   
+   // @Test
     public void testMarsLocation() throws IOException {
     	String mars = ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CCS", "Mars", 23);
     	
     	// Asserting for an empty String when the location is Mars
     	assertTrue(mars.isEmpty());
     	
+    	  	
+    }
+
+    // Question -2, This test case is to check the result should only return the location which has the required Type Of the charger ...   	
+    	
+    @Test
+    public void testForParticularType() throws IOException {
+    	
+    	String chargerDetailsCHAdeMo = ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CHAdeMo", "Durham", 23);
+    	assertTrue(chargerDetailsCHAdeMo.contains("CHAdeMo"));
+    	
+    	String chargerDetailsCCS = ChargerFinder.findBestCharger("https://tranquil-headland-85251.herokuapp.com", 64, "CCS", "Durham", 23);
+    	assertTrue(chargerDetailsCCS.contains("CCS"));
     }
     
     

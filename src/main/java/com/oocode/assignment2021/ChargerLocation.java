45a21df2f74629b621edb7f1e737012014d5ac04
Ankur Sand <sandankur@gmail.com>
2021-04-25 11:48:17 +0530

    Added 3 new instance variables which needs to be considered to get the best charger based on the disance and Time

--- src/main/java/com/oocode/assignment2021/ChargerLocation.java
+++ src/main/java/com/oocode/assignment2021/ChargerLocation.java
@@ -1,15 +1,25 @@
 package com.oocode.assignment2021;
 
-import java.util.List;
+import java.util.*;
 
 public class ChargerLocation {
     public final String name;
     public final int distance;
+    public final int amount;
+    public final int avgSpeed;
+    public final int chargingSpeed;
     public final List<Charger> chargers;
+   
 
-    public ChargerLocation(String name, int distance, List<Charger> chargers) {
+    public ChargerLocation(String name, int distance, int amount, int avgSpeed ,int chargingSpeed,List<Charger> chargers) {
         this.name = name;
         this.distance = distance;
+        this.amount = amount;
+        this.avgSpeed = avgSpeed;
         this.chargers = chargers;
+        this.chargingSpeed = chargingSpeed;
     }
+    
+
+    
 }
